8ab1f99324519df69041e6642c926722a14419c3
Kalló Bernát <kallo.bernat@gmail.com>
2011-05-24 19:58:50 +0200

    Backend working

--- backend/simple.pl
+++ backend/simple.pl
@@ -4,28 +4,44 @@
 
 start_backend :- 
     File = 'data/default.pl',
-    (file_exists(File) -> consult(File) ; true),
+    (file_exists(File) -> load_data(File) ; true),
     open(File, append, S),
     add_stream_alias(S, data_file).
+
+load_data(File) :-
+    open(File, read, S),
+    repeat,
+    read(S, X),
+    (X == end_of_file ->
+        true;
+        backend_nowrite(X), fail),
+    close(S).
 
 tables([wlist, witem]).
 
 check_valid(X) :-
-    X=..[Tab|_], 
+    X=..[Cmd,Row],
+    memberchk(Cmd, [query,assert,retract]),
+    Row=..[Tab|_], 
     tables(Tabs),
     memberchk(Tab,Tabs).
 
-backend(query(X)) :-
+backend_nowrite(X) :-
     check_valid(X),
-    X.
-backend(assert(X)) :- 
-    check_valid(X),
-    assertz(X),
-    write_canonical(data_file, X).
-backend(retract(X)) :-
-    check_valid(X),
-    retract(X).
-    
+    backend_execute(X).
 
+backend(X) :-
+    backend_nowrite(X),
+    backend_write(X).
+
+backend_execute(query(R)) :- R.
+backend_execute(assert(R)) :- assertz(R).
+backend_execute(retract(R)) :- retract(R).
+
+backend_write(Term) :-
+    write_canonical(data_file, Term),
+    write(data_file, '.'),
+    nl(data_file).
+    
 
 commit :- close(data_file).
